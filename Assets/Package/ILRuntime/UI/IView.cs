59ec172251c74b7492a376e408826e9f1dd22196
dinghuilin <632636416@qq.com>
2020-11-26 21:09:13 +0800

    提交Iview

---
+++ Assets/Package/ILRuntime/UI/IView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IView 
+{
+    void Awake();
+
+    void OnEnable();
+
+    void Start();
+
+    void Update();
+
+    void OnDisable();
+
+    void OnDestory();
+
+    void SetGameObject(GameObject go);
+
+    GameObject GetGameObject();
+}
